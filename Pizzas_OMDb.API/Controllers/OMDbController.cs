d8f460f31b6a54da48d2cb63a699e878129e2eb7
MarianoCaceres5 <46956818@comunidadort.edu.ar>
2023-03-26 15:53:18 -0300

    Falta formatear los valores que devuelve

--- Pizzas_OMDb.API/Controllers/OMDbController.cs
+++ Pizzas_OMDb.API/Controllers/OMDbController.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
 using System.Collections;
 using System;
 using System.Collections.Generic;
@@ -11,21 +14,23 @@ namespace Pizzas.API.Controllers {
     [ApiController]
     [Route("/api/[controller]")]
     public class OMDB : ControllerBase {
-
+        
         [HttpGet] 
         [Route("search")]       
         public IActionResult GetPeliculaByName ([FromQuery] string nombrePelicula) {
 
-            string apiResponse = async HTTPHelper.GetContentAsync("http://www.omdbapi.com/?t="+nombrePelicula+ "apikey=8f11e689", "error");
-            return Ok(apiResponse);
+            //string apiResponse = async HTTPHelper.GetContentAsync("http://www.omdbapi.com/?t="+nombrePelicula+ "apikey=8f11e689", "error");
+            //return Ok(apiResponse);
+            Task<string> apiResponse = HTTPHelper.ObtenerPeliculaPorTitulo(nombrePelicula, "error");
+            return Ok("la pelicula es " + apiResponse);
             
         }
 
         [HttpGet]
         [Route("movie/{idPelicula}")]   
         public IActionResult GetPeliculaById ([FromRoute] string idPelicula) {
-            return Ok("La pelicula enviada fue " + idPelicula);
-            
+            Task<string> apiResponse = HTTPHelper.ObtenerPeliculaPorId(idPelicula, "error");
+            return Ok("la pelicula es " + apiResponse);            
         }       
 
         
--- Pizzas_OMDb.API/Helpers/HTTPHelper.cs
+++ Pizzas_OMDb.API/Helpers/HTTPHelper.cs
@@ -1,25 +1,41 @@
-using System.Net.Http;
+using System.Threading.Tasks;
+using System;
 using System.Net;
 using System.Collections;
-using System;
 using System.Collections.Generic;
 using Pizzas.API.Helpers;
 using Pizzas.API.Models;
+using OMDb.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
-
+using System.Net.Http;
 
 namespace Pizzas.API.Helpers
 {
-    public static class HTTPHelper{
-        
-        public static async Task<string> GetContentAsync(string respuesta, string defaultValueOnError){
-            string returnValue = defaultValueOnError;            
-            HttpClient client = new HttpClient();
+    public static class HTTPHelper{        
+        public static async Task<string> ObtenerPeliculaPorTitulo(string respuesta, string defaultValueOnError){
+            string returnValue;  
+            string targetUrl = "https://www.omdbapi.com/?apikey=8f11e689&t=" + respuesta;
+            HttpClient client = new HttpClient();     
             try{
-                using (HttpResponseMessage response = await httpClient.GetAsync(targetURL)){
-                    returnValue = await response.Content.ReadAStringAsync();
+                using (HttpResponseMessage response = await client.GetAsync(targetUrl)){
+                    returnValue = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(returnValue);
+                }
+            } catch (Exception ex){
+                returnValue = defaultValueOnError;
+            }
+            return returnValue;
+        }
 
+        public static async Task<string> ObtenerPeliculaPorId(string respuesta, string defaultValueOnError){
+            string returnValue;  
+            string targetUrl = "https://www.omdbapi.com/?apikey=8f11e689&i=" + respuesta;
+            HttpClient client = new HttpClient();     
+            try{
+                using (HttpResponseMessage response = await client.GetAsync(targetUrl)){
+                    returnValue = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(returnValue);
                 }
             } catch (Exception ex){
                 returnValue = defaultValueOnError;
