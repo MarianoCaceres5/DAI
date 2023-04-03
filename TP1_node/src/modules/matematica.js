56e8d9750a9064448aea09a99a370b393ab3bfea
MarianoCaceres5 <46956818@comunidadort.edu.ar>
2023-04-03 20:55:29 -0300

    arranque tp1 node

---
+++ TP1_node/src/modules/matematica.js
@@ -0,0 +1,18 @@
+
+export let sumar = (num1, num2) =>{
+    return num1+num2;
+}
+
+export let restar = (num1, num2) =>{
+    return num1-num2;
+}
+
+export function multiplicar(num1, num2){
+    return num1*num2;
+}
+
+export function dividir(num1, num2){
+    return num1/num2;
+}
+
+export const PI = 3.14;
