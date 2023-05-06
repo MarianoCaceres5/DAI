815ba3d13ca8974aa7284a4d9b1e39f1face3968
MarianoCaceres5 <46956818@comunidadort.edu.ar>
2023-05-06 16:07:42 -0300

    actualice carpetas

--- TP_API_MSSQL/src/modules/log-helper.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-export default function CopiaError(textoError){
-    fs.appendFile('errorsFile.txt', textoError + "\n", function (err) {
-        if (err) throw err;
-        console.log('ERROR');
-    });
-}
