4bad020e56dc06a170ec3dbfa1c46661b9c49b2d
MARIANO CACERES SMOLER <46956818@ort>
2023-06-06 08:34:48 -0300

    reentregue con opcionales

--- TP_PizzaIngredientes/server-router.js
+++ TP_PizzaIngredientes/server-router.js
@@ -13,9 +13,33 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
 app.use(express.static('public'));
 
+const tiempoTranscurrido = function (req, res, next){
+    let tiempo1 = new Date();
+    console.log("Tiempo inicial: " + tiempo1.toISOString())
+    next();
+    let tiempo2 = new Date();
+    console.log("Tiempo final: " + tiempo2.toISOString())
+    console.log("Tiempo transcurrido: " + (tiempo2 - tiempo1).toString() + " milisegundo/s");
+}
+
+const checkApiKey = function  (req, res, next){
+    if(req.headers.apikey != undefined && req.headers.apikey != null && req.headers.apikey == "123456789"){
+        next();
+    }else{
+        res.status(401).send('Unauthorized, es necesario una ApiKey Valida');
+    }
+}
+
+const headerResponse = function (req, res, next){
+    res.set('CreatedBy', 'Mariano');
+    next();
+}
+
+app.use(tiempoTranscurrido);
+app.use(checkApiKey);
+app.use(headerResponse);
 app.use("/Pizzas", pizzaRouter)
 app.use("/Ingredientes", ingredientesRouter)
 app.use("/Unidades", unidadesRouter)
