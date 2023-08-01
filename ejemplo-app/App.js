e1f9d1d59eecff0f1a45a433dc8e115ddd28dee8
MarianoCaceres5 <46956818@comunidadort.edu.ar>
2023-08-01 20:38:41 -0300

    ejemplo app

---
+++ ejemplo-app/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <ImageBackground source={{uri:"https://i.pinimg.com/originals/c3/f8/b5/c3f8b5d3e9a61af0fcc7c67d5532ad38.png"}} resizeMode="cover" style={[styles.background]} >
+        <Text style={[styles.text]}>Lionel Fressi</Text>
+      </ImageBackground>      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background:{
+    width: '100%',
+    height: '95%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    backgroundColor: '#6CA5BF90', 
+    fontSize: 30,
+    padding: 20,
+    textAlign: 'center',
+    width: '100%'
+  }
+});
