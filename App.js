c0fb0ef980a83bdce1ed46910db78d10a81fc065
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:13:43 -0300

    ejemplo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ImageBackground source={{uri:"https://i.pinimg.com/originals/c3/f8/b5/c3f8b5d3e9a61af0fcc7c67d5532ad38.png"}} resizeMode="cover" style={[styles.background]} >
+        <Text style={[styles.text]}>Lionel Fressi</Text>
+      </ImageBackground>      
     </View>
   );
 }
@@ -17,4 +19,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background:{
+    width: '100%',
+    height: '95%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    backgroundColor: '#6CA5BF90', 
+    fontSize: 30,
+    padding: 20,
+    textAlign: 'center',
+    width: '100%'
+  }
 });
