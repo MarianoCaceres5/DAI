4d4660e939a11d5945095f95a64bc26c295e3b53
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 08:09:24 -0300

    ejemplo2 terminado

--- ejemplo-app2/App.js
+++ ejemplo-app2/App.js
@@ -1,24 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Pressable, StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Pressable, StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
 
-  const [text, setText] = useState('')
+  const [nombre, setNombre] = useState('')
+  const [telefono, setTelefono] = useState(0)
+  const [email, setEmail] = useState('')
+  const [clave, setClave] = useState('')
+
+  let showData = () => {
+
+    let data = {
+      nombre: nombre,
+      telefono: telefono,
+      email: email,
+      clave: clave
+    }    
+
+    console.log("DATA:", data)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
+      <Text style={[styles.textLabel]}>Nombre</Text>
       <TextInput
         editable
         maxLength={20}
         style={styles.input}
-        placeholder="Write some text"
-        value={text}
-        onChangeText={text => setText(text)}
+        placeholder="Ingrese su nombre"
+        value={nombre}
+        onChangeText={input => setNombre(input)}
       />  
-      <TouchableOpacity style={[styles.touchableOpacity]} onPress={() => setText('')} ><Text style={[styles.textButton]}>Reset Input</Text></TouchableOpacity>
-      <Pressable style={[styles.pressable]} onLongPress={() => setText('')}><Text style={[styles.textPressable]}>Long Press to Reset Input</Text></Pressable>
+      <Text style={[styles.textLabel]}>Telefono</Text>
+      <TextInput
+        editable
+        maxLength={20}
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Ingrese su telefono"
+        value={telefono}
+        onChangeText={input => setTelefono(input)}
+      />
+      <Text style={[styles.textLabel]}>Email</Text>
+      <TextInput
+        editable
+        maxLength={20}
+        style={styles.input}
+        placeholder="Ingrese su email"
+        value={email}
+        onChangeText={input => setEmail(input)}
+      />
+      <Text style={[styles.textLabel]}>Clave</Text>
+      <TextInput
+        editable
+        maxLength={20}
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Ingrese su clave"
+        value={clave}
+        onChangeText={input => setClave(input)}
+      />
+      <TouchableOpacity style={[styles.button1]} onPress={() => showData()} ><Text style={[styles.textButton]}>Log Data</Text></TouchableOpacity>
+      <Pressable style={[styles.button2]} onPress={() => showData()}><Text style={[styles.textPressable]}>Log Data</Text></Pressable>
+      <Button onPress={() => showData()} title="Log Data" color={'black'}/>
       
     </SafeAreaView>
   );
@@ -31,15 +77,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  touchableOpacity: {
+  button1: {
     marginTop: 10,
     width: '90%',
     height: 60,
     backgroundColor: 'black',
     borderRadius: 15
   },
-  pressable: {
+  button2: {
     marginTop: 10,
+    marginBottom: 10,
     width: '90%',
     height: 60,
     backgroundColor: 'white',
@@ -63,8 +110,14 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
+    width: '90%',
     margin: 12,
     borderWidth: 1,
     padding: 10,
   },
+  textLabel:{
+    alignSelf: 'flex-start',
+    marginLeft: '5%',
+    marginTop: 5
+  }
 });
