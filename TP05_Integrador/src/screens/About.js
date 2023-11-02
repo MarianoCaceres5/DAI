3b3437c7600e77c46b57e1b97d6e28b09e3be280
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-02 10:18:28 -0300

    carga video

--- TP05_Integrador/src/screens/About.js
+++ TP05_Integrador/src/screens/About.js
@@ -3,17 +3,12 @@ import React, { useState, useEffect } from 'react';
 import Menu from '../components/Menu'
 import DataService from '../services/DataService';
 import Boton from '../components/Boton'
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import qrImage from '../../assets/qr.png'
-import * as Font from 'expo-font';
-import BarcodeFont from '../../assets/fonts/BarcodeFont'
 
 let dataService = new DataService();
 
 export default function About({ navigation }) {
 
   const [image, setImage] = useState(null);
-  const [fontsLoaded, setFontsLoaded] = useState(false);
 
   let loadBackground = async () => {
     if (JSON.parse(await dataService.obtenerBackground())) {
@@ -22,33 +17,20 @@ export default function About({ navigation }) {
     }
   }
 
-  async function loadFonts() {
-    await Font.loadAsync({
-      'BarcodeFont': BarcodeFont,
-    });
-    setFontsLoaded(true);
-  }
-
   useEffect(() => {
     loadBackground();
-    loadFonts();
   }, []);
 
   return (
-    (fontsLoaded ? (
-      <>
-        <SafeAreaView style={[styles.container]}>
-          <ImageBackground source={{ uri: image }} style={styles.image}>
-            <Text style={styles.qrFont}>Mariano Caceres</Text>
-            <Text style={{ backgroundColor: 'white', fontSize: 20, width: '80%', textAlign: 'center' }}>Presione para copiar el texto</Text>
-          </ImageBackground>
-          <Menu navigation={navigation} />
-        </SafeAreaView>
-      </>
-    ) : (
-      <></>
-    ))
-
+    <>
+      <SafeAreaView style={[styles.container]} >
+        <ImageBackground source={{ uri: image }} style={styles.image}>
+          <Text>Mariano Caceres</Text>
+          <Text style={{ backgroundColor: 'white', fontSize: 20, width: '80%', textAlign: 'center' }}>Presione para copiar el texto</Text>
+        </ImageBackground>
+        <Menu navigation={navigation} />
+      </SafeAreaView>
+    </>
   )
 }
 
@@ -60,9 +42,6 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     backgroundColor: '#fff'
-  },
-  qrFont: {
-    fontFamily: 'BarcodeFont'
   },
   button: {
     marginTop: 20,
--- TP05_Integrador/src/screens/UrlPlayer.js
+++ TP05_Integrador/src/screens/UrlPlayer.js
@@ -1,31 +1,71 @@
-import { View, Text, StyleSheet, SafeAreaView, ImageBackground } from 'react-native'
-import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ImageBackground, Button } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react';
 import Menu from '../components/Menu'
 import DataService from '../services/DataService';
+import { Video, ResizeMode } from 'expo-av';
 
 let dataService = new DataService();
 
-export default function UrlPlayer({navigation}) {
+export default function UrlPlayer({ navigation }) {
 
+  const video = useRef(null);
+  const [status, setStatus] = React.useState({});
   const [image, setImage] = useState(null);
+  const [videoUrl, setVideo] = useState(undefined);
+  const [musica, setMusica] = useState();
 
   let loadBackground = async () => {
-    if(JSON.parse(await dataService.obtenerBackground())){
+    if (JSON.parse(await dataService.obtenerBackground())) {
       let backgroundImage = JSON.parse(await dataService.obtenerBackground());
       setImage(backgroundImage.uri);
-    }    
+    }
+  }
+
+  let loadMultimedia = async () => {
+    if (await dataService.obtenerDatos()) {
+      let datos = await dataService.obtenerDatos();
+      let video = datos.video;
+      console.log(video)
+      setVideo(video)
+      let musica = datos.musica;
+      setMusica(musica)
+    }
   }
 
   useEffect(() => {
-    loadBackground();  
+    loadBackground();
+    loadMultimedia();
   }, []);
 
   return (
     <SafeAreaView style={[styles.container]}>
-      <ImageBackground source={{uri: image}} style={styles.image}>
-        <Text style={{backgroundColor:'white', fontSize: 20, width: '80%', textAlign:'center'}}>UrlPlayer</Text>
+      <ImageBackground source={{ uri: image }} style={styles.image}>
+        <Text style={{ backgroundColor: 'white', fontSize: 20, width: '80%', textAlign: 'center' }}>UrlPlayer</Text>
+        {videoUrl ? (
+          <>
+            <Video
+              style={styles.video}
+              ref={video}
+              source={{
+                uri: videoUrl,
+              }}              
+              useNativeControls
+              resizeMode={ResizeMode.CONTAIN}
+              isLooping
+              onPlaybackStatusUpdate={status => setStatus(() => status)}
+            />
+            <Button
+              title={status.isPlaying ? 'Pause' : 'Play'}
+              onPress={() =>
+                status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+              }
+            />
+          </>
+        ) : (
+          <></>
+        )}
       </ImageBackground>
-      <Menu navigation={navigation}/>
+      <Menu navigation={navigation} />
     </SafeAreaView>
   )
 }
@@ -45,4 +85,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  video: {
+    width: '80%',
+    height: 200
+  }
 });
